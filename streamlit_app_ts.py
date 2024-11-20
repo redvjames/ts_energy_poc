f16f545d0d82f3e88bce82d8e91548f1927d0bc0
James Verdan <112069712+redvjames@users.noreply.github.com>
2024-11-20 17:34:41 +0800

    Update streamlit_app_ts.py

--- streamlit_app_ts.py
+++ streamlit_app_ts.py
@@ -126,8 +126,11 @@ transform_path = Path(os.environ.get('BUILDINGS_BENCH', '')) / 'metadata' / 'tra
 
 st.write(os.getcwd())
 st.write(os.listdir('/mount/src/ts_energy_poc'))
-st.write(os.listdir('/home/adminuser/venv/lib/python3.11/site-packages'))
+# st.write(os.listdir('/home/adminuser/venv/lib/python3.11/site-packages'))
 st.write(os.listdir('/home/adminuser/venv/lib/python3.11/site-packages/buildings_bench/'))
+st.write(os.listdir('/home/adminuser/venv/lib/python3.11/site-packages/buildings_bench/models'))
+st.write(os.listdir('/home/adminuser/venv/lib/python3.11/site-packages/buildings_bench/configs'))
+st.write(os.listdir('/home/adminuser/venv/lib/python3.11/site-packages/buildings_bench/data'))
 
 # Get the file path of the module
 import buildings_bench
