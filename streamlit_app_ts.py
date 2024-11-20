4545efd3e2648d4a5aada01ccd31eee977285b38
James Verdan <112069712+redvjames@users.noreply.github.com>
2024-11-20 15:11:46 +0800

    Update streamlit_app_ts.py

--- streamlit_app_ts.py
+++ streamlit_app_ts.py
@@ -18,6 +18,8 @@ from buildings_bench.evaluation import aggregate, scoring_rule_factory
 from buildings_bench.models import model_factory
 from torch.utils.data import ConcatDataset, Subset
 
+st.write(!ls)
+
 try:
     df_input = pd.read_csv('./data/data.csv', index_col=0)
 except FileNotFoundError:
